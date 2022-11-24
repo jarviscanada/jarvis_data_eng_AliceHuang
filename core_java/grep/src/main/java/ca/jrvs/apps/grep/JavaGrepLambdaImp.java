93b07f86c7a0feba30fd3dca2909b3f604399fc7
aliceh233 <alicehuang233@gmail.com>
2022-11-24 02:10:03 +0000

    finished grepLambda implementations

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepLambdaImp.java
@@ -3,8 +3,15 @@ package ca.jrvs.apps.grep;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JavaGrepLambdaImp implements JavaGrep {
 
@@ -26,27 +33,69 @@ public class JavaGrepLambdaImp implements JavaGrep {
   }
   @Override
   public void process() throws IOException {
-
+    List<String> matchedLines = new ArrayList<>();
+    listFiles(getRootPath()).stream().forEach(f->{
+      readLInes(f).stream().filter(this::containsPattern).forEach(matchedLines::add);
+    });
+    writeToFile(matchedLines);
   }
 
   @Override
   public List<File> listFiles(String rootDir) {
-    return null;
+    File directoryPath = null;
+    List<File> fileArrayList = new ArrayList<>();
+    try {
+      directoryPath = new File(rootDir);
+    } catch (Exception e)  {
+      logger.error("Unable to load files", e);
+    } finally {
+      File fileList[] = directoryPath.listFiles();
+      assert fileList != null;
+      Stream.of(fileList).forEach(f-> {
+        if (f.isFile()) {
+          fileArrayList.add(f);
+        } else {
+          fileArrayList.addAll(listFiles(f.getAbsolutePath()));
+        }
+      });
+      return fileArrayList;
+    }
   }
 
   @Override
   public List<String> readLInes(File inputFile) {
-    return null;
+    List<String> stringList;
+    Stream<String> lines = null;
+    try {
+      lines = Files.lines(inputFile.toPath());
+
+    } catch (Exception e) {
+      logger.error("Unable to read file", e);
+    }
+
+    assert lines != null;
+    stringList = lines.collect(Collectors.toList());
+    return stringList;
   }
 
   @Override
   public boolean containsPattern(String Line) {
-    return false;
+    Pattern pattern = Pattern.compile(getRegex(),Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(Line);
+    return matcher.find();
   }
 
   @Override
   public void writeToFile(List<String> lines) throws IOException {
-
+    FileWriter myWriter = new FileWriter(getOutFile());
+    lines.stream().map(String::valueOf).forEach(l-> {
+      try {
+        myWriter.write(l);
+      } catch (Exception e) {
+        logger.error("Unable to write file", e);
+      }
+    });
+    myWriter.close();
   }
 
   @Override
