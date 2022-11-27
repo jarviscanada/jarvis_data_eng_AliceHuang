e596ee2428eac984f01d75ba9d59fd7b24f9683c
aliceh233 <alicehuang233@gmail.com>
2022-11-27 19:55:02 +0000

    init jdbc executor and util files

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,8 +1,28 @@
 package ca.jrvs.apps.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JDBCExecutor {
 
+  final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
   public static void main(String[] args) {
-    System.out.println("Hello,World!");
+    JDBCExecutor jdbcExecutor = new JDBCExecutor();
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+        "hplussport", "postgres", "password");
+    try {
+      Connection connection = dcm.getConnection();
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+      while(resultSet.next()){
+        System.out.println(resultSet.getInt(1));
+      }
+    } catch (SQLException e) {
+      jdbcExecutor.logger.error("unable to execute", e);
+    }
   }
 }
