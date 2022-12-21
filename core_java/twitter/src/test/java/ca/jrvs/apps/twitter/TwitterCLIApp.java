d0d681f0a62c303e915b6ccb2e03294a8f2d1563
aliceh233 <alicehuang233@gmail.com>
2022-12-21 02:51:25 +0000

    twitterCLIApp class

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/TwitterCLIApp.java
@@ -0,0 +1,81 @@
+package ca.jrvs.apps.twitter;
+
+import ca.jrvs.apps.twitter.controller.TwitterController;
+import ca.jrvs.apps.twitter.dao.TwitterDao;
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.service.TwitterService;
+import ca.jrvs.apps.twitter.utils.Controller;
+import ca.jrvs.apps.twitter.utils.CrdDao;
+import ca.jrvs.apps.twitter.utils.JsonParser;
+import ca.jrvs.apps.twitter.utils.Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gdata.util.common.base.PercentEscaper;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwitterCLIApp {
+  public  static final String USAGE = "USAGE: TwitterCLIApp post|show|delete [options]";
+  private Controller controller;
+  private final Logger logger = LoggerFactory.getLogger(TwitterCLIApp.class);
+
+  public TwitterCLIApp(Controller controller) {
+    this.controller = controller;
+  }
+
+  public static void main(String[] args) throws Exception{
+    String consumerKey = System.getenv("consumerKey");
+    String consumerSecret = System.getenv("consumerSecret");
+    String accessToken = System.getenv("accessToken");
+    String tokenSecret = System.getenv("tokenSecret");
+
+    HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret, accessToken, tokenSecret);
+
+    CrdDao dao = new TwitterDao(httpHelper);
+    Service service = new TwitterService(dao);
+    Controller controller = new TwitterController(service);
+    TwitterCLIApp twitterCLIApp = new TwitterCLIApp(controller);
+
+    //start
+    twitterCLIApp.run(args);
+  }
+
+  public void run(String[] args)
+      throws UnsupportedEncodingException, URISyntaxException, NoSuchFieldException, IllegalAccessException {
+    if (args.length == 0) {
+      throw new IllegalArgumentException(USAGE);
+    }
+    switch (args[0].toLowerCase()) {
+      case "post":
+        printTweet(controller.postTweet(args));
+        break;
+      case "show":
+        printTweet(controller.showTweet(args));
+        break;
+      case  "delete":
+        controller.deleteTweet(args).forEach(this::printTweet);
+      default:
+        throw new IllegalArgumentException(USAGE);
+    }
+  }
+
+  private void printTweet(Tweet tweet) {
+    try {
+      System.out.println(JsonParser.toJson(tweet, true, false));
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Unable to convert tweet object to string", e);
+    }
+  }
+
+}
